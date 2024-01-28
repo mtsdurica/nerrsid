6eb07cc5830dc39b10db31bda40e8ed417c1f061
Matúš Ďurica <mtsdurica@gmail.com>
2024-01-28 22:23:17 +0100

    feat: Start implementing cursor

---
+++ NERRSID/Cursor.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+class Cursor
+{
+private:
+	bool CursorSetFlag;
+	int PositionXCoordinate;
+	int PositionYCoordinate;
+	// path to destination
+public:
+	Cursor();
+	~Cursor();
+	bool GetCursorSetFlag();
+	int GetPositionXCoordinate();
+	int GetPositionYCoordinate();
+
+	void GetCursorSetFlag(bool cursorSetFlag);
+	void SetPositionXCoordinate(int positionXCoordinate);
+	void SetPositionYCoordinate(int positionYCoordinate);
+};
